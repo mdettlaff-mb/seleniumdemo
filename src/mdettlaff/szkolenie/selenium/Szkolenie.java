b7c0b4e91a434a0ee696dffed434b6ad1123e98f
Michał Dettlaff <mdettlaff@jitsolutions.pl>
2015-01-03 15:04:41 +0100

    convert to junit test

--- src/mdettlaff/szkolenie/selenium/Szkolenie.java
+++ src/mdettlaff/szkolenie/selenium/Szkolenie.java
@@ -1,30 +1,50 @@
 package mdettlaff.szkolenie.selenium;
 
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.firefox.FirefoxDriver;
-import org.openqa.selenium.interactions.Actions;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
 
 public class Szkolenie {
 
-	public static void main(String[] args) throws Exception {
-		WebDriver driver = new FirefoxDriver();
-		try {
-			driver.get("http://store.demoqa.com/");
-			// WebElement search = driver.findElement(By.name("s"));
-			// search.sendKeys("ipad");
-			// search.sendKeys(Keys.ENTER);
-			// search.submit();
-			WebElement productCategory = driver.findElement(By.xpath("//*[@id=\"menu-item-33\"]/a"));
-			Actions actions = new Actions(driver);
-			actions.moveToElement(productCategory);
-			actions.build().perform();
-//			JavascriptExecutor ex = (JavascriptExecutor) driver;
-//			ex.executeScript("arguments[0].click();", productCategory);
-			Thread.sleep(1000);
-		} finally {
-			driver.close();
-		}
+	private WebDriver driver;
+
+	@Before
+	public void setUp() {
+		driver = new FirefoxDriver();
+	}
+
+	@After
+	public void tearDown() {
+		driver.quit();
+	}
+
+	@Test
+	public void testStore() {
+		driver.get("http://store.demoqa.com/");
+		WebElement search = driver.findElement(By.name("s"));
+		search.sendKeys("ipad");
+		search.submit();
+		// WebElement productCategory =
+		// driver.findElement(By.xpath("//*[@id=\"menu-item-33\"]/a"));
+		// Actions actions = new Actions(driver);
+		// actions.moveToElement(productCategory);
+		// actions.build().perform();
+		// JavascriptExecutor ex = (JavascriptExecutor) driver;
+		// ex.executeScript("arguments[0].click();", productCategory);
+		WebElement ipadLink = driver.findElement(By.linkText("Apple iPad 6 32GB (White, 3D)"));
+		ipadLink.click();
+		WebElement addToCartButton = driver.findElement(By.name("Buy"));
+		addToCartButton.click();
+		// driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+		By goToCheckoutLinkLocator = By.linkText("Go to Checkout");
+		new WebDriverWait(driver, 10).until(ExpectedConditions.elementToBeClickable(goToCheckoutLinkLocator));
+		WebElement goToCheckoutLink = driver.findElement(goToCheckoutLinkLocator);
+		goToCheckoutLink.click();
 	}
 }
